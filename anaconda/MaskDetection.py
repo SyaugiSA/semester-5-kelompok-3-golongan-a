3615694ffe6372e7fdc05380e08db7e0df127e82
wahyudianpratiwi1 <71805820+wahyudianpratiwi1@users.noreply.github.com>
2021-12-29 15:00:35 +0700

    Update MaskDetection.py

--- anaconda/MaskDetection.py
+++ anaconda/MaskDetection.py
@@ -7,65 +7,72 @@ import lockdoor
 
 model = load_model('model-019.model')
 
-face_cascade = cv2.CascadeClassifier('cascade.xml')
+face_cascade = cv2.CascadeClassifier('cascade.xml') ##Memanggil Hasil Training
 mouth_cascade = cv2.CascadeClassifier('Mouth.xml')
 bw_threshold = 80
 
 count = 0
-font = cv2.FONT_HERSHEY_SIMPLEX
-org = (30, 30)
-weared_mask_font_color = (255, 0, 0)
-not_weared_mask_font_color = (0, 0, 255)
-noface = (255, 255, 255)
-thickness = 2
-font_scale = 1
+font = cv2.FONT_HERSHEY_SIMPLEX  ##tipe font
+org = (30, 30) ##Koordinat X dan Y Untuk Menampilkan Font Yang Akan Muncul
+weared_mask_font_color = (255, 0, 0) ##warna font menggunakan masker
+not_weared_mask_font_color = (0, 0, 255) ##warna font tidak menggunakan masker
+noface = (255, 255, 255) ##warna font tidak terdeteksi wajah
+thickness = 2  ##ketebalan
+font_scale = 1  ##skala font
 weared_mask = "Menggunakan Masker"
 not_weared_mask = "Tidak Menggunakan Masker"
 
-##cap = cv2.imread('')
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-
+cap = cv2.VideoCapture("WhatsApp Video 2021-12-28 at 16.36.15.mp4") ##Menginisilisasi Input 
 while True:
     # mematikan lampu dan lock door
     lockdoor.reset()
 
-    ret, img = cap.read() ##Membaca Input
+    ret, img = cap.read() ##Membaca Input Yang Telah Diinisilalisasi Inputnya
     img = cv2.flip(img,1)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Mengkonfersi gambar menjadi grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Proses Mengkonfersi gambar menjadi grayscale
     (thresh, black_and_white) = cv2.threshold(gray, bw_threshold, 255, cv2.THRESH_BINARY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    faces_bw = face_cascade.detectMultiScale(black_and_white, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4) ##Untuk Memulai Mendeteksi MultiScale Gray
+    faces_bw = face_cascade.detectMultiScale(black_and_white, 1.1, 4) ##Untuk Memulai Mendeteksi MultiScale Black and WHite
 
     if(len(faces) == 0 and len(faces_bw) == 0):
         cv2.putText(img, "Tidak Terdeteksi Wajah...", org, font, font_scale, noface, thickness, cv2.LINE_AA)
+        ##Menampilkan Teks Ketika Tidak Terdeteksi Wajah dengan menentukan tipe font, ukuran font tidak terdeteksi wajah,
+        ##Dan warna font, ketebalan font
     elif(len(faces) == 0 and len(faces_bw) == 1):
         cv2.putText(img, weared_mask, org, font, font_scale, weared_mask_font_color, thickness, cv2.LINE_AA)
-        # terbuka
+        
+        ##Menampilkan Teks Ketika Menggunakan Masker dengan menentukan tipe font, ukuran font ketika memakai masker,
+        ##warna font, ketebalan font
+        
+        # Membuka kunci
         lockdoor.terbuka()
 
     else:
         for (x, y, w, h) in faces:
             
-            face_img = gray[y:y+w,x:x+w]
-            resized = cv2.resize(face_img,(100,100))
-            normalized = resized/255.0
+            face_img = gray[y:y+w,x:x+w] ##Image dengan kumpulan Titik Koordinatnya
+            resized = cv2.resize(face_img,(100,100)) ## Image Di resize atau diperkecil
+            normalized = resized/255.0 
             reshaped = np.reshape(normalized,(1,100,100,1))
             result = model.predict(reshaped)
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2) ##jika inputnya cocok maka membounding
-            roi_gray = gray[y:y + h, x:x + w] ## ROI gray dengan koordinatnya
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2) ##jika inputnya cocok maka membounding wajah
+            roi_gray = gray[y:y + h, x:x + w] ## ROI gray dengan titik koordinatnya
             roi_color = img[y:y + h, x:x + w] ## ROI color dengan titik koordinatnya
             mouth_rects = mouth_cascade.detectMultiScale(gray, 1.5, 5) 
 
         if(len(mouth_rects) == 0):
             cv2.putText(img, weared_mask, org, font, font_scale, weared_mask_font_color, thickness, cv2.LINE_AA)
+            ##Menampilkan Teks Ketika Menggunakan Masker dengan menentukan tipe font, ukuran font ketika memakai masker,
+            ##Dan warna font, ketebalan font
 
         else:
             for (mx, my, mw, mh) in mouth_rects:
                 if(y < my < y + h):
                     cv2.putText(img, not_weared_mask, org, font, font_scale, not_weared_mask_font_color, thickness, cv2.LINE_AA)
-                    # tidak terbuka
+                    ##Menampilkan Teks Ketika Tidak Menggunakan Masker dengan menentukan tipe font, ukuran font ketika tidak memakai masker,
+                    ##warna font, ketebalan font
+                    
+                    # Kunci tidak terbuka
                     lockdoor.tidakTerbuka()
 
                     print("Image"+str(count)+"Tersimpan") ##Menyimpan Gambar
@@ -74,10 +81,10 @@ while True:
                     count += 1 ##Dengan memasukkan gambar dengan angka ditambah 1
                     break
                 
-    cv2.imshow('Mask Detection', img)
+    cv2.imshow('Mask Detection', img) ##Menampilkan display dengan nama Mask Detection
     k = cv2.waitKey(30) & 0xff 
     if k == 27:
         break
     
-cap.release()
-cv2.destroyAllWindows()
+cap.release() ##Mengeksekusi Program
+cv2.destroyAllWindows() ##Mengakhiri Semua Window
